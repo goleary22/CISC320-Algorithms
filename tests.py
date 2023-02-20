a53d415fff5c5ba7a9b287f97715db48ea1b3d16
Grace O'Leary <goleary@udel.edu>
2023-02-19 22:07:18 -0500

    Update tests.py

--- tests.py
+++ tests.py
@@ -4,6 +4,7 @@ import answer
 class TestAnswer(unittest.TestCase):
 
     def test_sum(self):
+        #testing just positive values
         actual = answer.summation(["2\n", "1\n", "3\n"])
         self.assertEqual(actual,6)
 
@@ -23,6 +24,6 @@ class TestAnswer(unittest.TestCase):
         actual = answer.summation(["-999\n", "3\n"])
         self.assertEqual(actual,"EMPTY")
 
-        #testing -999 break
-        actual = answer.summation (["4\n", "6\n", "3\n", "-999\n", "9\n"])
-        self.assertEqual(actual,13)
+        #testing -999 break AND negative
+        actual = answer.summation (["4\n", "-6\n", "3\n", "-999\n", "9\n"])
+        self.assertEqual(actual,7)
